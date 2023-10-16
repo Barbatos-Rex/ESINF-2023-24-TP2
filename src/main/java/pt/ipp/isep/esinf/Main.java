98a1da9985a2784f5c2c479faf46ed3b33dad2ae
André Gomes <1190387@isep.ipp.pt>
2023-10-16 23:42:43 +0100

    Add ex5 with debug capabilities, but complexity is tied to ex6. Added text file output

--- src/main/java/pt/ipp/isep/esinf/Main.java
+++ src/main/java/pt/ipp/isep/esinf/Main.java
@@ -10,41 +10,65 @@ import pt.ipp.isep.esinf.struct.auxiliary.TripStartEnd;
 import pt.ipp.isep.esinf.struct.auxiliary.TripVehicle;
 import pt.ipp.isep.esinf.struct.auxiliary.VehicleTrips;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TreeCluster result = new Importer().importData();
+        File f = new File("./log.txt");
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+
+        PrintWriter pw = new PrintWriter(f);
 
         Search s = new Search(result);
+
+        pw.println("<!--Execute exercise 1 (No relevant parameters to add)-->");
 
         Optional<TripVehicle> tv = s.findTripAndVehicle(561);
         Optional<VehicleTrips> vt = s.findVehicleAndTripsById(108);
 
         if (tv.isPresent()) {
             System.out.println(tv.get());
+            pw.println(tv.get());
         }
 
         System.out.println();
 
         if (vt.isPresent()) {
             System.out.println(vt.get());
+            pw.println(vt.get());
         }
 
-
+        pw.println("<!--Execute exercise 2 (Between days: 150 - 156)-->");
         MaxMinAverageOfTripByType ex2 = result.getTripTree().getMaxMinAvgBetween(150, 156, result.getVehicleTree());
         ex2.updateValues();
         System.out.println(ex2);
+        System.out.println("\n");
+        pw.println(ex2);
+        pw.println("\n");
 
+
+        pw.println("<!--Execute exercise 3 (Trips range: 1500 - 1555)-->");
         Map<Trip, TripStartEnd> ex3 = result.getTripTree().obtainTripStartEnd(1500, 1555);
         for (TripStartEnd value : ex3.values()) {
             System.out.println(value);
             System.out.println("----------------------------------------------------------");
+            pw.println(value);
+            pw.println("----------------------------------------------------------");
         }
+        System.out.println("\n");
+        pw.println("\n");
 
-
+        pw.println("<!--Execute exercise 4 (Vehicles searched: 130 , 131, 132, 550 and 554)-->");
         Map<Integer, Trip> ex4 = result.getTripTree().findLongestTripByVehicleIds(Set.of(130, 131, 132, 550, 554));
         System.out.println();
         for (Map.Entry<Integer, Trip> integerTripEntry : ex4.entrySet()) {
@@ -54,23 +78,64 @@ public class Main {
             System.out.println("Trip Distance: " + String.format("%.3f", integerTripEntry.getValue().tripDistance()) + " km");
             System.out.println();
             System.out.println("//////////////////////////////////////////////////////////////////////////////");
+
+            pw.println();
+            pw.println("VehicleId: " + integerTripEntry.getKey());
+            pw.println("Longest Trip: " + integerTripEntry.getValue().getId().getId());
+            pw.println("Trip Distance: " + String.format("%.3f", integerTripEntry.getValue().tripDistance()) + " km");
+            pw.println();
+            pw.println("//////////////////////////////////////////////////////////////////////////////");
         }
+        System.out.println("\n");
+        pw.println("\n");
+
+        pw.println("<!--Execute exercise 5 ((42.24882,-83.76743139) to (43.25883,-84.76743125))-->");
+        Trip ex5 = result.getTripTree().generateCoordenate2DTree().obtainClosestCoordenatesToOriginDestination(
+                TimeCoordenates.genCoordWithoutTime("42.24882", "-83.76743139"),
+                TimeCoordenates.genCoordWithoutTime("43.25883", "-84.76743125"));
+        System.out.println("Trip: "+ex5);
+        System.out.println("Start: "+ex5.getEntries().getFirst().getCoordenates());
+        System.out.println("End: "+ex5.getEntries().getLast().getCoordenates());
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        pw.println("Trip: "+ex5);
+        pw.println("Start: "+ex5.getEntries().getFirst().getCoordenates());
+        pw.println("End: "+ex5.getEntries().getLast().getCoordenates());
+        pw.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 
 
+        pw.println("<!--Execute exercise 5 ((42.24882,-83.76743139) to (43.25883,-84.76743125) top 10 longest trips)-->");
         Set<Trip> ex6 = result.getTripTree().generateCoordenate2DTree().topNLongestTripsBetweenInArea(
                 TimeCoordenates.genCoordWithoutTime("42.24882", "-83.76743139"), TimeCoordenates.genCoordWithoutTime("43.25883", "-84.76743125"), 10);
 
 
-        int count=1;
+        int count = 1;
         for (Trip trip : ex6) {
-            System.out.println("("+count+"º)Trip: ");
+            System.out.print("(" + count + "º)Trip: ");
             System.out.println(trip);
             System.out.print("\nTraveld Distance: ");
             System.out.println(trip.tripRealDistance());
             System.out.println("#####################################################################################");
+
+
+            pw.print("(" + count + "º)Trip: ");
+            pw.println(trip);
+            pw.print("\nTraveld Distance: ");
+            pw.println(trip.tripRealDistance());
+            pw.println("#####################################################################################");
+
             count++;
         }
 
+        pw.println("\n\nDate: "+new SimpleDateFormat("dd-MM-yyyy HH:mm:ss:SSS").format(new Date()));
+        pw.flush();
+        pw.close();
+
 
     }
+
+
+
+
+
 }
--- src/main/java/pt/ipp/isep/esinf/struct/simple/Trip2DTree.java
+++ src/main/java/pt/ipp/isep/esinf/struct/simple/Trip2DTree.java
@@ -3,16 +3,75 @@ package pt.ipp.isep.esinf.struct.simple;
 import pt.ipp.isep.esinf.domain.trip.TimeCoordenates;
 import pt.ipp.isep.esinf.domain.trip.Trip;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Trip2DTree extends TwoDTree<Trip> {
-//    public Pair<Pair<Double, Double>, Pair<Double, Double>> obtainClosestCoordenatesToOriginDestination(
-//            TimeCoordenates start, TimeCoordenates end) {
-//
-//
-//    }
+
+
+    private class ClosestToStartAndEndComparator implements Comparator<Trip> {
+
+
+        private class DebugEntry {
+            private Trip trip;
+            private double val;
+
+            public DebugEntry(Trip trip, double val) {
+                this.trip = trip;
+                this.val = val;
+            }
+
+            @Override
+            public String toString() {
+                return "DebugEntry{" +
+                        "trip=" + trip.getId() +
+                        ", val=" + val +
+                        '}';
+            }
+        }
+
+        private TimeCoordenates start;
+        private TimeCoordenates end;
+
+        private Set<DebugEntry> debugMap = new TreeSet<>(new Comparator<DebugEntry>() {
+            @Override
+            public int compare(DebugEntry o1, DebugEntry o2) {
+                return Double.compare(o1.val,o2.val);
+            }
+        });
+
+        public ClosestToStartAndEndComparator(TimeCoordenates start, TimeCoordenates end) {
+            this.start = start;
+            this.end = end;
+        }
+
+
+        private double totalDistance(Trip trip) {
+            double d = start.distance(trip.getEntries().first().getCoordenates()) + end.distance(trip.getEntries().last().getCoordenates());
+            debugMap.add(new DebugEntry(trip, d));
+            return d;
+        }
+
+        @Override
+        public int compare(Trip o1, Trip o2) {
+            return Double.compare(totalDistance(o1), totalDistance(o2));
+        }
+
+        public Set<DebugEntry> getDebugMap() {
+            return debugMap;
+        }
+    }
+
+
+    public Trip obtainClosestCoordenatesToOriginDestination(
+            TimeCoordenates start, TimeCoordenates end) {
+        Set<Trip> trips = new HashSet<>();
+        topNLongestTripsBetweenInArea(start, end, getRoot(), trips);
+        ClosestToStartAndEndComparator comparator = new ClosestToStartAndEndComparator(start, end);
+        TreeSet<Trip> result = new TreeSet<>(comparator);
+        result.addAll(trips);
+        Set<ClosestToStartAndEndComparator.DebugEntry> debugSet = comparator.getDebugMap();
+        return result.first();
+    }
 
 
     public Set<Trip> topNLongestTripsBetweenInArea(TimeCoordenates start, TimeCoordenates end, int n) {
@@ -20,7 +79,7 @@ public class Trip2DTree extends TwoDTree<Trip> {
         Set<Trip> result = new TreeSet<>(new Trip.TripRealDistanceComparator());
         topNLongestTripsBetweenInArea(start, end, getRoot(), tmp);
         for (Trip trip : tmp) {
-            if (n==0){
+            if (n == 0) {
                 break;
             }
             result.add(trip);
